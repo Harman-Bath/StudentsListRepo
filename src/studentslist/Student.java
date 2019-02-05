c6fcca24d92600216205be18fdde298f88d62abf
Harman-Bath <46941565+Harman-Bath@users.noreply.github.com>
2019-02-05 16:28:04 -0500

    Added date to header

--- src/studentslist/Student.java
+++ src/studentslist/Student.java
@@ -1,15 +1,7 @@
 package studentslist;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//FEB 5TH, 2019
 
-/**
- *
- * @author shaim
- */
 public class Student {
    
     private String DoB; 
